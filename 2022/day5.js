a36a46b2bdb943221d36c934094fda1d345b30d9
Roland Pilinyi <85487129+LifeTiltz@users.noreply.github.com>
2023-08-27 23:08:25 +0100

    forgot the \r prob on windows

--- 2022/day5.js
+++ 2022/day5.js
@@ -1,6 +1,6 @@
-// let a =  
+// let a =
 // `
-// [B]             [B] [S]        
+// [B]             [B] [S]
 // [M]             [P] [L] [B] [J]
 // [D]     [R]     [V] [D] [Q] [D]
 // [T] [R] [Z]     [H] [H] [G] [C]
@@ -10,64 +10,66 @@
 // [B] [L] [Q] [W] [S] [L] [J] [W] [Z]
 // 1   2   3   4   5   6   7   8   9`
 
-// [D]    
-// [N] [C]    
+// [D]
+// [N] [C]
 // [Z] [M] [P]
-//  1   2   3 
-const fs = require("fs")
+//  1   2   3
+const fs = require("fs");
 
-let egy = ["z","n"]
-let ketto = ["m","c","d"]
-let harom = ["p"]
-
-let one = ["B","W","N","P","T","D","M","B"]
-let two = ["L","Z","S","W","R"]
-let three = ["Q","H","Z","J","Z","R"]
-let four = ["W","D","V","B"]
-let five = ["S","H","M","J","H","V","P","B"]
-let six = ["L","G","N","F","H","D","L","S"]
-let seven = ["J","Q","Z","J","G","Q","B"]
-let eight = ["W","S","F","S","C","D","J"]
-let nine = ["Z","W","M"]
+let egy = ["z", "n"];
+let ketto = ["m", "c", "d"];
+let harom = ["p"];
 
 const whichOne = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six",
-    "7": "seven",
-    "8": "eight",
-    "9": "nine"
-}
+  1: ["B", "W", "N", "P", "T", "D", "M", "B"],
+  2: ["L", "Z", "S", "W", "R"],
+  3: ["Q", "H", "Z", "J", "Z", "R"],
+  4: ["W", "D", "V", "B"],
+  5: ["S", "H", "M", "J", "H", "V", "P", "B"],
+  6: ["L", "G", "N", "F", "H", "D", "L", "S"],
+  7: ["J", "Q", "Z", "J", "G", "Q", "B"],
+  8: ["W", "S", "F", "S", "C", "D", "J"],
+  9: ["Z", "W", "M"],
+};
+
+// const whichOne = {
+//   1: "one",
+//   2: "two",
+//   3: "three",
+//   4: "four",
+//   5: "five",
+//   6: "six",
+//   7: "seven",
+//   8: "eight",
+//   9: "nine",
+// };
 
 const whichOneTest = {
-    "1": ["z","n"],
-    "2": ["m","c","d"],
-    "3": ["p"],
-}
+  1: ["z", "n"],
+  2: ["m", "c", "d"],
+  3: ["p"],
+};
 
 const findEndState = () => {
-    const lines = fs.readFileSync("day5test.txt", { encoding: "utf-8" })
-    .split("\n")
+  const lines = fs
+    .readFileSync("./rawData/day5.txt", { encoding: "utf-8" })
+    .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+    .trim() // Remove starting/ending whitespace
+    .split("\n"); // Split on newline
 
-    lines.map((line) => {
-        let broken = line.split(" ")
-        const move = broken[1]
-        const from = broken[3]
-        const to = broken[5]
+  lines.map((line) => {
+    let broken = line.split(" ");
+    const move = broken[1];
+    const from = broken[3];
+    const to = broken[5];
 
-        for (let i = 0; i < move; i++) {
-            let last = whichOneTest[from].at(-1)
-            console.log(whichOneTest[to]);
-            whichOneTest[to].push(last)
-            console.log(last,whichOneTest[to]);
+    for (let i = 0; i < move; i++) {
+      let last = whichOne[from].at(-1);
+      whichOne[to].push(last);
+      whichOne[from].pop();
+    }
+  });
 
-            whichOneTest[from].pop()
-        }
-    })
-
-    console.log(whichOneTest);
-}
-findEndState()
+  console.log(whichOne);
+};
+findEndState();

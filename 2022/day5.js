c6390778c8d3fac4011f9cb91bd92dcc44fc351c
Roland Pilinyi <85487129+LifeTiltz@users.noreply.github.com>
2023-08-28 15:57:10 +0100

    DAY5 - bad input fixed
    Should have made a parser for the input. Wasnt faster quickly typing because it seems the input changed

--- 2022/day5.js
+++ 2022/day5.js
@@ -1,16 +1,17 @@
 
+const { log } = require("console");
 const fs = require("fs");
 
 const whichOne = {
-  1: ["B", "W", "N", "P", "T", "D", "M", "B"],
-  2: ["L", "Z", "S", "W", "R"],
-  3: ["Q", "H", "Z", "J", "Z", "R"],
-  4: ["W", "D", "V", "B"],
-  5: ["S", "H", "M", "J", "H", "V", "P", "B"],
-  6: ["L", "G", "N", "F", "H", "D", "L", "S"],
-  7: ["J", "Q", "Z", "J", "G", "Q", "B"],
-  8: ["W", "S", "F", "S", "C", "D", "J"],
-  9: ["Z", "W", "M"],
+  1: ["B", "W", "N"],
+  2: ["L", "Z", "S", "P", "T", "D", "M", "B"],
+  3: ["Q", "H", "Z", "W", "R"],
+  4: ["W", "D", "V", "J", "Z", "R"],
+  5: ["S", "H", "M", "B"],
+  6: ["L", "G", "N", "J", "H", "V", "P", "B"],
+  7: ["J", "Q", "Z", "F", "H", "D", "L", "S"],
+  8: ["W", "S", "F", "J", "G", "Q", "B"],
+  9: ["Z", "W", "M", "S", "C", "D", "J"],
 };
 
 const whichOneTest = {
@@ -38,18 +39,49 @@ const findEndState = () => {
       whichOne[from].pop();
     }
   });
+  
+  // for (let i = 0; i < lines.length; i++) {
+  //   let line = lines[i]
+
+  //   let broken = line.split(" ");
+  //   const move = broken[1];
+  //   const from = broken[3];
+  //   const to = broken[5];
+
+  //   for (let j = 0; j < move; j++) {
+  //     let last = whichOne[from].at(-1);
+
+  //     if (i >= i) {
+  //       console.log("_____________");
+  //       console.log({move, from, to, last, i});
+  //       console.log(whichOne);
+  //       console.log("- - -");
+  //     }
+
+  //     whichOne[to].push(last);
+  //     whichOne[from].pop();
+
+  //     if (i >= i) {
+  //       console.log(whichOne);
+  //       console.log("___________");
+  //     }
+
+  //   }
+  //}
 
   console.log(whichOne);
 
-//   let answer = []
+  let answer = []
+  let testt = ""
 
-//   for (const key in whichOne) {
-//     console.log("whichOne[key]", whichOne[key]);
-//     console.log({key}, answer[key]);
-//     answer[key-1] = whichOne[key].pop()
-// }
+  for (const key in whichOne) {
+    console.log("whichOne[key]", whichOne[key]);
+    console.log({key}, answer[key]);
+    answer[key-1] = whichOne[key].pop()
+}
 
-// console.log({answer});
+console.log({answer});
+
 
 };
 findEndState();
